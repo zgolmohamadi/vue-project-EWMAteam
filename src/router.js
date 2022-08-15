a7c786a51e05dbc282f93d6aa2086b37f2553443
zgolmohamadi <35527928+zgolmohamadi@users.noreply.github.com>
2022-08-15 22:18:47 +0430

    first commit

--- src/router.js
+++ src/router.js
@@ -4,11 +4,11 @@ import MovieDetail from "./pages/movies/MovieDetail.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/vue-project-EWMAteam/'),
   routes: [
     { path: "/", redirect: "/moviesList" },
     { path: "/moviesList", component: MoviesList, name:"home", props: true },
-    { path: "/movie/:id", component: MovieDetail, props: true },
+    { path: "/movie/:id", component: () => import('./pages/movies/MovieDetail.vue'), props: true },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
