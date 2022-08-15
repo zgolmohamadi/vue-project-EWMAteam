3f6353ae717be49c9f88b24f9fd712b0f0db9f99
zgolmohamadi <35527928+zgolmohamadi@users.noreply.github.com>
2022-08-15 22:37:58 +0430

    change packagejson

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MoviesList from "./pages/movies/MoviesList.vue";
-import MovieDetail from "./pages/movies/MovieDetail.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
